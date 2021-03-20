42f983418eca1bbcf257d8e6890973c22a64a9ab
9hello-stranger6 <31783698+9hello-stranger6@users.noreply.github.com>
2021-03-21 01:19:17 +0300

    1-3

--- js/script.js
+++ js/script.js
@@ -63,3 +63,24 @@ window.addEventListener("keydown", function (evt) {
         }
     }
 });
+
+const mapPopup = document.querySelector(".map-wrapper");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
